0222a2ae6c07de54cdc3df70b1e25d6d4282ffcb
heverton100 <ton1451@hotmail.com>
2020-07-31 10:01:48 -0300

    Tela de Splash Concluída

--- app/src/main/java/com/example/myapplication/SplashActivity.kt
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -1,11 +1,27 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        changeToLogin()
+    }
+
+    fun changeToLogin(){
+        val intent = Intent (this,MainActivity::class.java)
+
+        Handler().postDelayed({
+            intent.change()
+        },2000)
+    }
+
+    fun Intent.change(){
+        startActivity(this)
+        finish()
     }
 }
